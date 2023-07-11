917a03d110b67051782ddedef9eeb51a8e358b82
JimmyKimunyi <jimmykimunyi@gmail.com>
2023-07-11 09:42:49 +0300

    uibuild

--- 19/index.js
+++ 19/index.js
@@ -43,7 +43,9 @@ app.get("/api/persons/:id", (req, res, next) => {
 
 app.get("/info", (req, res) => {
   const time = new Date().toString();
-  res.send(`<p>  Phonebook has info for ${persons.length} <br /> ${time} </p>`);
+  res.send(
+    `<p> Phonebook has info for : ${persons.length} <br /> ${time} </p>`
+  );
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
